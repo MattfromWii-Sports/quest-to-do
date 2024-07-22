f4f7c32df19f7541b5a9bdaad1deec68b0730de2
Matt <matthewjosefcruz@gmail.com>
2024-07-22 16:36:14 +0800

    Set Layout Design For Mobile, And Create Hamburger Menu Logic

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import './styles.css';
 import menu from './images/menu.svg';
 
 console.log(menu);
+
+//for hamburger icon logic
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+let hamburgerToggle = false;
+hamburgerIcon.addEventListener('click', () => {
+    let mobileWidth = 700; //remember to match it with css media queries
+    if(window.innerWidth >=  700) {
+        return; //not mobile :(
+    }
+    if(hamburgerToggle === true) {
+        hamburgerIcon.classList.remove('open');
+        hamburgerToggle = false;
+    } else {
+        hamburgerIcon.classList.add('open');
+        hamburgerToggle = true;
+    }
+});
